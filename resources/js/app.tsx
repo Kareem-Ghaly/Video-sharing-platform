3f06b51a90808e1ee3986334348c73436cebac1a
Kareem-Ghaly <km2236833@gmail.com>
2026-09-23 17:19:13 +0300

    Configuring Jetstream

---
+++ resources/js/app.tsx
@@ -0,0 +1,51 @@
+import '../css/app.css';
+
+
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { Toaster } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { initializeTheme } from '@/hooks/use-appearance';
+import AppLayout from '@/layouts/app-layout';
+import AuthLayout from '@/layouts/auth-layout';
+import SettingsLayout from '@/layouts/settings/layout';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+void createInertiaApp({
+    title: (title) => (title ? `${title} - ${appName}` : appName),
+    resolve: async (name) => {
+        const pages = import.meta.glob('./pages/**/*.tsx');
+        const page = await pages[`./pages/${name}.tsx`]();
+        return (page as any).default;
+    },
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(
+            <TooltipProvider delayDuration={0}>
+                <App {...props} />
+                <Toaster />
+            </TooltipProvider>
+        );
+    },
+    layout: (name) => {
+        switch (true) {
+            case name === 'welcome':
+                return null;
+            case name.startsWith('auth/'):
+                return AuthLayout;
+            case name.startsWith('settings/'):
+            case name.startsWith('teams/'):
+                return [AppLayout, SettingsLayout];
+            default:
+                return AppLayout;
+        }
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
+
+// This will set light / dark mode on load...
+initializeTheme();
