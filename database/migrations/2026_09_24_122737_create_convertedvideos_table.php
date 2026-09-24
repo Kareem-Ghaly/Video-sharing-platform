0f89de642ba6c8a974f17c8b957ea61773ca3d89
Kareem-Ghaly <km2236833@gmail.com>
2026-09-24 15:29:10 +0300

    Add Convertedvideo Model and Table

---
+++ database/migrations/2026_09_24_122737_create_convertedvideos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('convertedvideos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(column: 'video_id')->references(column: 'id')->on(table: 'videos')
+        ->onDelete(action: 'cascade');
+        $table->string(column: 'mp4_Format_240')->nullable();
+        $table->string(column: 'mp4_Format_360')->nullable();
+        $table->string(column: 'mp4_Format_480')->nullable();
+        $table->string(column: 'mp4_Format_720')->nullable();
+        $table->string(column: 'mp4_Format_1080')->nullable();
+        $table->string(column: 'webm_Format_240')->nullable();
+        $table->string(column: 'webm_Format_360')->nullable();
+        $table->string(column: 'webm_Format_480')->nullable();
+        $table->string(column: 'webm_Format_720')->nullable();
+        $table->string(column: 'webm_Format_1080')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('convertedvideos');
+    }
+};
